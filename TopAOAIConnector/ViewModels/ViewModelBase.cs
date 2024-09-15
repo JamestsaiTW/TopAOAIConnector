583985f0496f7ca7c4d4f9a854fdb78d904de298
James Tsai <jatsai@msn.com>
2024-09-15 22:39:45 +0800

    feat: Init project

---
+++ TopAOAIConnector/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TopAOAIConnector.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ TopAOAIConnector/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TopAOAIConnector.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
