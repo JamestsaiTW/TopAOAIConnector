aae16ac26a056e2f33e93c2cb6e214f3f1fdc755
James Tsai <jatsai@msn.com>
2024-09-23 01:41:59 +0800

    feat: Add basic settingpage UI

---
+++ TopAOAIConnector/Models/AOAISettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TopAOAIConnector.Models;
+
+internal class AOAISettings
+{
+    public string? Endpoint { get; set; }
+    public string? SecretKey1 { get; set; }
+    public string? SecretKey2 { get; set; }
+    public string? DeployModelName { get; set; }
+}
--- TopAOAIConnector/ViewModels/SettingPageViewModel.cs
+++ TopAOAIConnector/ViewModels/SettingPageViewModel.cs
@@ -1,5 +1,27 @@
-﻿namespace TopAOAIConnector.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.IO;
+using TopAOAIConnector.Models;
+
+namespace TopAOAIConnector.ViewModels;
 
 internal partial class SettingPageViewModel : ViewModelBase
 {
+    private readonly string settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "TopAOAIConnector", "settings.json");
+
+    [ObservableProperty]
+    private AOAISettings? _settings;
+
+    [RelayCommand]
+    private void Clear()
+    {
+        System.Diagnostics.Debug.WriteLine($"Clear...");
+    }
+
+    [RelayCommand]
+    private void Save()
+    {
+        System.Diagnostics.Debug.WriteLine($"Save to...{settingsPath}");
+    }
 }
