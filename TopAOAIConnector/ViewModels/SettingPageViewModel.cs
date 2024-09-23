9a7876dc77de675861dc7c7d4d47dcb14674d268
James Tsai <jatsai@msn.com>
2024-09-24 01:04:58 +0800

    fix: Implement settingpageviewmodel

--- TopAOAIConnector/ViewModels/SettingPageViewModel.cs
+++ TopAOAIConnector/ViewModels/SettingPageViewModel.cs
@@ -2,13 +2,32 @@
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 using TopAOAIConnector.Models;
 
 namespace TopAOAIConnector.ViewModels;
 
 internal partial class SettingPageViewModel : ViewModelBase
 {
-    private readonly string settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "TopAOAIConnector", "settings.json");
+    private const string directoryName = "TopAOAIConnector";
+    private const string settingsFileName = "settings.json";
+
+    private readonly string settingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), directoryName, settingsFileName);
+
+    public SettingPageViewModel()
+    {
+        if (File.Exists(settingsFilePath))
+        {
+            var jsonSettings = File.ReadAllText(settingsFilePath);
+            var aoaiSettings = JsonSerializer.Deserialize<AOAISettings>(jsonSettings);
+            Settings = aoaiSettings;
+        }
+        else
+        {
+            Settings = new();
+        }
+    }
 
     [ObservableProperty]
     private AOAISettings? _settings;
@@ -17,11 +36,33 @@ internal partial class SettingPageViewModel : ViewModelBase
     private void Clear()
     {
         System.Diagnostics.Debug.WriteLine($"Clear...");
+
+        if (File.Exists(settingsFilePath))
+        {
+            File.Delete(settingsFilePath);
+        }
+
+        var directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), directoryName);
+        if (Directory.Exists(directoryPath))
+        {
+            Directory.Delete(directoryPath);
+        }
+
+        Settings = new();
     }
 
     [RelayCommand]
-    private void Save()
+    private async Task Save()
     {
-        System.Diagnostics.Debug.WriteLine($"Save to...{settingsPath}");
+        System.Diagnostics.Debug.WriteLine($"Save to...{settingsFilePath}");
+
+        var directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), directoryName);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        var jsonSettings = JsonSerializer.Serialize(Settings);
+        await File.WriteAllTextAsync(settingsFilePath, jsonSettings);
     }
 }
